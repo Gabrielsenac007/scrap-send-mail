0140ec63eaa227c092eac4027341950c7db5b49a
Gabriel <gabrielvdcsilva@gmail.com>
2024-11-12 00:01:52 -0300

    mail

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
 
 const url = 'https://bakashi.tv/calendario/';
 
@@ -30,6 +32,47 @@ async function main() {
     console.log(data);
 
     await browser.close();
+
+       
+    const topThreeAnimes = data.slice(0, 3);
+
+    
+    const emailContent = `Episódios novos:\n\n` + topThreeAnimes.map((anime, index) => {
+        return `#${index + 1}\nTítulo: ${anime.title}\nAno: ${anime.year}\nURL: ${anime.url}\n`;
+    }).join('\n\n');
+
+    
+    await sendEmail(emailContent);
+
 }
+
+
+async function sendEmail(content) {
+    
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: process.env.EMAIL_USER, 
+            pass: process.env.EMAIL_PASS 
+        }
+    });
+
+    
+    const mailOptions = {
+        from: EMAIL_USER,
+        to: 'gabrielvdcsilva@gmail.com',
+        subject: 'Episódios novos disponíveis!',
+        text: content 
+    };
+
+    
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Email enviado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao enviar email:', error);
+    }
+}
+
 
 main();
